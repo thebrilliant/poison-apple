2c00bc3c0308f31ac17848a515cfc715c9c4a99c
v <vivyanw@uw.edu>
2015-12-03 19:31:32 -0800

    added controllers and custom cells

---
+++ FinalProject/StoryCell.swift
@@ -0,0 +1,26 @@
+//
+//  StoryCell.swift
+//  FinalProject
+//
+//  Created by iGuest on 12/3/15.
+//  Copyright © 2015 Derry Cheng. All rights reserved.
+//
+
+import UIKit
+
+class StoryCell: UITableViewCell {
+    
+    @IBOutlet weak var storyImage: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
