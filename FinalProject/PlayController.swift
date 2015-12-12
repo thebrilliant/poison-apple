a0f567aa8aa7ea876bfe9521f9a5d9cfabbd5a53
v <vivyanw@uw.edu>
2015-12-11 17:18:22 -0800

    audio is magically workinggit status

--- FinalProject/PlayController.swift
+++ FinalProject/PlayController.swift
@@ -18,22 +18,22 @@ class PlayController: UIViewController {
     @IBOutlet weak var option1: UIButton!
     @IBOutlet weak var option2: UIButton!
     
-    /*//var sourcePath = NSBundle.mainBundle().pathForResource("kpws", ofType: "mp3")!
-    var sourcePath = NSBundle.mainBundle().pathForResource("kwps", ofType: "mp3")
-    var filePath : NSURL = NSURL(string: "music/kwps.mp3", relativeToURL: NSURL(string: NSBundle.mainBundle().pathForResource("kwps", ofType: "mp3")!))!
+    //var sourcePath = NSBundle.mainBundle().pathForResource("kpws", ofType: "mp3")!
+//    var sourcePath = NSBundle.mainBundle().pathForResource("kwps", ofType: "mp3")
+//    var filePath : NSURL = NSURL(string: "music/kwps.mp3", relativeToURL: NSURL(string: NSBundle.mainBundle().pathForResource("kwps", ofType: "mp3")!))!
+//    
+//    var audio = try? AVAudioPlayer(contentsOfURL: NSURL(string: "music/kwps.mp3", relativeToURL: nil)!, fileTypeHint: "mp3")
     
-    var audio = try? AVAudioPlayer(contentsOfURL: NSURL(string: "music/kwps.mp3", relativeToURL: nil)!, fileTypeHint: "mp3")
-    
-    //var backgroundAudio = try? AVAudioPlayer(contentsOfURL: NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("kpws", ofType: "mp3")!))
-    */
+    var backgroundAudio = try? AVAudioPlayer(contentsOfURL: NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("kwps", ofType: "mp3")!))
+
     
     var appTheme : UIColor!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         /*print("source path: \(sourcePath)")
-        print("file path: \(filePath)")
-        audio!.play()*/
+        print("file path: \(filePath)")*/
+        backgroundAudio!.play()
         // Do any additional setup after loading the view.
     }
 
