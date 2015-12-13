d4794e85b07d6d0a3fdd9da5d6635ed4b61f71af
Susan-Wolfgram <skwolf@uw.edu>
2015-12-13 05:15:38 -0800

    read some firebase data

--- FinalProject/NavViewController.swift
+++ FinalProject/NavViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import AVFoundation
+import Firebase
 
 struct images {
     var snowWhite : [UIImage]
@@ -18,12 +19,27 @@ class NavViewController: UINavigationController {
     var appTheme : UIColor = UIColor.whiteColor()
     var appThemeName = 0
     var playerName : String = String()
+    var items = [NSDictionary]()
+    var myRootRef = Firebase(url:"https://swift-sw.firebaseio.com/")
     var backgroundAudio = try? AVAudioPlayer(contentsOfURL: NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("kwps", ofType: "mp3")!))
     var imagesSource = images(snowWhite : [UIImage(named: "snow-white-profile")!, UIImage(named: "evil-queen-profile")!], redRidingHood : [UIImage(named: "snow-white-profile")!, UIImage(named: "evil-queen-profile")!])
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+            myRootRef.observeEventType(.Value, withBlock: { snapshot in
+                var tempItems = [NSDictionary]()
+                    for item in snapshot.children {
+                        let child = item as! FDataSnapshot
+                        let dict = child.value as! NSDictionary
+        
+                        tempItems.append(dict)
+                    }
+        
+                    self.items = tempItems
+                    var foo = [String:AnyObject]()
+                    foo = self.items[0]["Page1"]! as! [String : AnyObject]
+                    print(foo["Text"]!)
+            })
         // Do any additional setup after loading the view.
     }
 
